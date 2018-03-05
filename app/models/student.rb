9eff9731151a6f6e437ae74cc027742bfa05071a
David Hormaza <dehormazah@unal.edu.co>
2018-03-05 21:01:12 +0000

    final

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
 class Student < ApplicationRecord
     
-    has_many:student_careers
-    has_many:careers, through:student_careers
+    has_many :student_careers
+    has_many :careers, through: :student_careers
+    
+    validates: first_name, last_name, presence: true, length: {´
+    maximum: 15}
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180305202541) do
+
+  create_table "careers", force: :cascade do |t|
+    t.string "idt"
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_careers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
